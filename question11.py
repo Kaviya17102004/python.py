0136f597a330a79b6f8c683dd216ec8e68f8d3d0
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-10-03 09:43:48 +0530

    question11.py

---
+++ question11.py
@@ -0,0 +1,10 @@
+a=int(input("tamil"))
+b=int(input("English"))
+c=int(input("Maths"))
+d=int(input("Science"))
+e=int(input("Social"))
+Average=a+b+c+d+e/5
+if(Average>35):
+    print("Additional class is required")
+else:
+    print("You are good to go")
