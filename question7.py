507072e4a43102e6bf9d48bd700ce0f84c07721f
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-10-03 09:34:35 +0530

    question7.py

---
+++ question7.py
@@ -0,0 +1,13 @@
+score=int (input("enter the mark: "))
+if(score<35):
+    print("Poor student")
+if(score>35& score<70):
+    print("Average student")
+if(score>70):
+    print("Good student")score=int (input("enter the mark: "))
+if(score<35):
+    print("Poor student")
+if(score>35& score<70):
+    print("Average student")
+if(score>70):
+    print("Good student")
