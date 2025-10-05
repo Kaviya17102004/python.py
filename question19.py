448621a5dee2f26ba151801cd72ea7540fdb7924
Kaviya K <kaviyapythondeveloper@gmail.com>
2025-10-05 18:57:18 +0530

    question19.py

---
+++ question19.py
@@ -0,0 +1,13 @@
+def passorfail(b):
+    
+    if(a>35 ):
+        print("pass")
+    else:
+        print("fail")
+    if(a%2==0):
+        print("even")
+    else:
+         print("odd")
+
+a=int(input("enter a mark :"))
+passorfail(a)
